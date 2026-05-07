222158cd575be66a1b043b0fcb5507757d55335a
ZCooldudeguy <731256@student.dusd.net>
2026-05-07 15:23:53 -0700

    e
    finished most everything

--- Unit5FinnellP7/Assets/Scrripes/GameManager.cs
+++ Unit5FinnellP7/Assets/Scrripes/GameManager.cs
@@ -14,15 +14,12 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
     public Button restartButton;
+    public GameObject titleScreen;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isGameActive = true;
-        score = 0;
-
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
+      
     }
 
     // Update is called once per frame
@@ -59,4 +56,17 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
 }
